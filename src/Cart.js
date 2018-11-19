27c6a54deb69b5a2ddf4de1f617a3ea3494425d0
nicholasgriffen <nicholas.s.griffen@gmail.com>
2018-11-19 14:50:17 -0700

    parent Cart component

---
+++ src/Cart.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import CartHeader from './CartHeader'
+import CartItems from './CartItems'
+import CartFooter from './CartFooter'
+
+class Cart extends Component {
+    render() {
+        return (
+            <div className="Cart">
+                <header>
+                    <CartHeader />
+                </header>
+                <CartItems />
+                <footer>
+                    <CartFooter copyright='2016'/>
+                </footer>
+            </div>
+        )
+    }
+}
+
+export default Cart
