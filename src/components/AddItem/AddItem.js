eaea2aa1a4a350f006e04ff553a0bd0421915ba5
nicholasgriffen <nicholas.s.griffen@gmail.com>
2018-11-20 15:14:56 -0700

    handle submit event

--- src/components/AddItem/AddItem.js
+++ src/components/AddItem/AddItem.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react'
 
 class AddItem extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            quantity: 1,
+            productID: this.props.products[0].id
+        }
+    }
+    onChange = e => {
+        this.setState({
+            ...this.state,
+            [e.target.name]: +e.target.value,
+        })
+    }
     onSubmit = e => {
-        console.log(e.target.value)
+        e.preventDefault()
+        const { quantity, productID } = this.state
+        const product = this.props.products.find(product => product.id === productID)
+        console.log(quantity, product)
     }
     render() {
         const { products } = this.props
         return (
             <form onSubmit={this.onSubmit}>
                 <label>Quantity:
-                <input type="number"/>
+                <input type="number" min="1" name="quantity" onChange={this.onChange} value={this.state.quantity}/>
                 </label>
                 <label>Item: 
-                <select>
+                <select name="productID" onChange={this.onChange}>
                     {products.map((product, idx) => (
                         <option key={idx} value={product.id}>{product.name}</option>
                     ))}
